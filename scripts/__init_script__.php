226730ff8332fcf6b373378e119e6fa5b2130409
Jakub Vrana <jakub@vrana.cz>
2013-07-24 15:57:00 -0700

    Allow running Diviner on Windows
    Summary: Also print error if libphutil is not found.
    
    Test Plan: Ran it on Windows twice.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, Korvin
    
    Differential Revision: https://secure.phabricator.com/D6504

--- scripts/__init_script__.php
+++ scripts/__init_script__.php
@@ -1,8 +1,9 @@
 <?php
 
 $include_path = ini_get('include_path');
-ini_set('include_path', $include_path.':'.dirname(__FILE__).'/../../');
-@require_once 'libphutil/src/__phutil_library_init__.php';
+$include_path .= PATH_SEPARATOR.dirname(__FILE__).'/../../';
+ini_set('include_path', $include_path);
+@include_once 'libphutil/src/__phutil_library_init__.php';
 if (!@constant('__LIBPHUTIL__')) {
   echo "ERROR: Unable to load libphutil. Update your PHP 'include_path' to ".
        "include the parent directory of libphutil/.\n";
